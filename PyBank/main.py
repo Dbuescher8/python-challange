2f8ab1a0789f78e6d849bcb8e189f18b76982a41
Dbuescher8 <buescher26@ymail.com>
2021-02-09 22:52:55 -0600

    added files

--- PyBank/main.py
+++ PyBank/main.py
@@ -21,13 +21,39 @@ with open(file_path) as csvfile:
         print(Month_total)
         Net_total_PL = Net_total_PL + float(row[1])
         print(str(Net_total_PL))
-        average_profit_loss
-        moNum = float(row[1])
-        change = moNum - profit
-        moChanges.append(change)
-        chgProfits = chgProfits + change
-        profit = moNum
+        date =row[0]
+        profit = float(row[1])
+        if (profit> Greatest_Increase["amount"]) 
+            Greatest_Increase[date] = date
+            Greatest_Increase[amount] = profit
+        if (profit < greatest_decrease["amount"]):
+            greatest_decrease["date"] = date
+            greatest_decrease["amount"] = profit
 
+
+print("Financial Analysis")
+print(---------------------------)
+print(f"Total Months: {Month_total}"")
+print(
+    f"Greatest Increase in Profits: {greatest_increase['date']} (${greatest_increase['amount']})")
+
+
+
+
+
+
+
+
+out_file = "./Analysis/output.txt"
+with open(out_file, 'w') as outputFile:
+    outputFile.write("Financial Analysis")
+    outputFile.write("----------------------------")
+    outputFile.write(f"Total Months: {Month_total}")
+    outputFile.write(f"Total: ${int(Net_total_PL)}")
+    outputFile.write(f"Average Change: ${str(round(avg_final,2))}")
+    outputFile.write(f"Greatest Increase in Profits: {incrDate} (${int(grIncr)})")
+    outputFile.write(f"Greatest Decrease in Losses: {decrDate} (${int(grDecr)})")
+    outputFile.write("----------------------------")
 
 
 #result should look like this 
