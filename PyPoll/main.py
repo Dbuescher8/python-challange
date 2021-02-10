8a8b4ee796a3d91f949cc7eb77bcf8f63a47cd14
Dbuescher8 <buescher26@ymail.com>
2021-02-09 23:24:29 -0600

    added files

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -20,6 +20,21 @@ import csv
 file_path = "PyPoll/Resources/election_data.csv"
 
 
+    if row[2] == "Khan": 
+            khan_votes +=1
+    elif row[2] == "Correy":
+            correy_votes +=1
+    elif row[2] == "Li": 
+            li_votes +=1
+    elif row[2] == "O'Tooley":
+            otooley_votes +=1
+
+candidate_options = ["Kahn", "Correy", "Li", "O'Tooley"]
+votes = [kkhan_votes, correy_votes,li_votes,otooley_votes ]
+
+dict_candidates_and_votes = dict(zip(candidates,votes))
+key = max(dict_candidates_and_votes, key=dict_candidates_and_votes.get)
+
 
 
 # Print the summary table
@@ -28,7 +43,7 @@ print(f"----------------------------")
 print(f"Total Votes: {vote_total}")
 print(f"----------------------------")
 print(f"Khan: {khan_percent:.3f}% ({khan_votes})")
-print(f"Correy: {correy_percent:.3f}% ({correy_votes})")
+print(f"Correy: {correy_percent:.3f}% ({khan_votes})")
 print(f"Li: {li_percent:.3f}% ({li_votes})")
 print(f"O'Tooley: {otooley_percent:.3f}% ({otooley_votes})")
 print(f"----------------------------")
